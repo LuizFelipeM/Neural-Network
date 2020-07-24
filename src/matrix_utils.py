052f8b6a1e65341ad7dc474eb062d8c7cdeb801c
LuizFelipeM <muniz.mourafelipe@gmail.com>
2020-07-24 13:44:03 -0300

    Add matrices length verification

--- src/matrix_utils.py
+++ src/matrix_utils.py
@@ -1,8 +1,24 @@
 def matrix_sum(*matrices):
-    value = 0
+    value = matrices[0].copy()
+    biggestLen = 0
 
-    for row in matrices[0]:
-        for cell in row:
-            value += cell
+    for matrix in matrices:
+        for row in matrix:
+            if len(row) > biggestLen:
+                biggestLen = len(row)
+
+    for matrix in matrices:
+        for row in matrix:
+            if len(row) != biggestLen:
+                raise Exception("Error - matrices has different lengths")
+
+    for matrix in matrices:
+        if matrices.index(matrix) == 0:
+            continue
+
+        for row in value:
+            rowMatrix = matrix[value.index(row)]
+            for cell in rowMatrix:
+                value[value.index(row)][rowMatrix.index(cell)] += cell
 
     return value
