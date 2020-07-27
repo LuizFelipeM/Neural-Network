21da3b700573caf6c9ba5e2001860f7d7f735a12
LuizFelipeM <muniz.mourafelipe@gmail.com>
2020-07-27 12:41:55 -0300

    Creating multiplication function for matrices

--- src/matrix_utils.py
+++ src/matrix_utils.py
@@ -8,7 +8,10 @@ def sum(*matrices):
     dims = lengths(matrices[0])
     for matrix in matrices:
         if dims[0] != lengths(matrix)[0] or dims[1] != lengths(matrix)[1]:
-            raise Exception("Matrices' lengths do not match! \n\n" + str(lengths(matrix)) + str(lengths(matrices[0])))
+            raise Exception("Matrices' lengths do not match! \n\n"
+                            + str(lengths(matrix))
+                            + str(lengths(matrices[0]))
+                            )
 
     result = []
     for matrix in matrices:
@@ -27,3 +30,28 @@ def sum(*matrices):
                 result[index] += matrix[index]
 
     return result
+
+def mult(matrice1, matrice2):
+    dims1 = lengths(matrice1)
+    dims2 = lengths(matrice2)
+
+    if dims1[1] != dims2[0]:
+        raise Exception("Matrices' row and column does not match! \n\n"
+                        + str(lengths(matrice1))
+                        + str(lengths(matrice2))
+                        )
+
+    result = [[0]*dims2[1]]*dims1[0]
+
+    for row in range(len(matrice1)):
+        for row2 in range(len(matrice2)):
+            for cell in range(len(matrice2[row2])):
+                result[row][cell] += matrice1[row][cell] * matrice2[cell][row2]
+
+    return result
+
+    # [1, 2]    [-1, 3]
+    # [3, 4] x  [ 4, 2]
+
+
+x = mult([[2, 3], [0, 1], [-1, 4]], [[1, 2, 3], [-2, 0, 4]])
