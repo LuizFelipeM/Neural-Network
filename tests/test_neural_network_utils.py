40d85e897b32372203237aac7ca18bb0cc2e5e3e
LuizFelipeM <muniz.mourafelipe@gmail.com>
2020-08-24 18:46:45 -0300

    Neural network utils tests adjust

--- tests/test_neural_network_utils.py
+++ tests/test_neural_network_utils.py
@@ -6,11 +6,12 @@ from src.activation_enum import ActivationEnum
 class TestNeuralNetworkUtils(unittest.TestCase):
     def test_activation_relu(self):
         val = 0.5
-        res = activation(ActivationEnum.relu, val)
-        self.assertEqual(res, val)
+        expected = 0.5
+        result = activation(ActivationEnum.relu, val)
+        self.assertEqual(result, expected)
 
     def test_activation_sigmoid(self):
         val = 5
         expected = 0.9933071490757153
-        res = activation(ActivationEnum.sigmoid, val)
-        self.assertEqual(res, expected)
+        result = activation(ActivationEnum.sigmoid, val)
+        self.assertEqual(result, expected)
